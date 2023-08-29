7057a22095d4547de4ad9e8ec3f5b38d45205d3f
Grace Olafare <oluwafunhmi@gmail.com>
2023-08-29 07:06:02 +0000

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,14 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
+
+app.get("/api/whoami", (req, res) => {
+    res.json({
+        "ipaddress": req.socket.remoteAddress,
+        "language": req.get('accept-language'),
+        "software": req.get('user-agent')
+    });
+})
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
